73342f7d4f89136462110087e61cda5a5bc6ad02
Theresa Wilkin <theresawilkin@yahoo.com>
2016-12-21 08:36:04 -0500

    movies sorting correctly

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -7,14 +7,31 @@ const initialState = {
   error: null
 };
 
+const sortMovies = (movies) => {
+  movies = movies.sort(function(a,b){
+    let result = (new Date(a.release_date) - new Date(b.release_date));
+    if (result === 0) {
+      if (a.media_type === 'tv') {
+        result = -1;
+      } else if (b.media_type === 'tv') {
+        result = 1;
+      } else if (a.media_type === 'season') {
+        result = -1;
+      } else if (b.media_type === 'season') {
+        result = 1;
+      }
+    }
+    return result;
+  });
+  return movies;
+}
+
 const moviesReducer = (state=initialState, action) => {
   if (action.type === actions.FETCH_MOVIES_REQUEST) {
     return Object.assign({}, state, {loading: true});
   }
   else if (action.type === actions.FETCH_MOVIES_SUCCESS) {
-        let movies = action.movies.sort(function(a,b){
-    return (new Date(a.release_date) - new Date(b.release_date));
-  });
+    let movies = sortMovies(action.movies);
     return Object.assign(
       {},
       state,
@@ -39,17 +56,15 @@ const moviesReducer = (state=initialState, action) => {
   }
   else if (action.type === actions.SEARCH_MOVIES_SUCCESS) {
     let movies = action.movies.results.map(movie => {
-        return {
-          id: movie.id,
-          media_type: movie.media_type,
-          title: movie.name || movie.title,
-          poster_path: movie.poster_path,
-          release_date: movie.first_air_date || movie.release_date
-        };
+      return {
+        id: movie.id,
+        media_type: movie.media_type,
+        title: movie.name || movie.title,
+        poster_path: movie.poster_path,
+        release_date: movie.first_air_date || movie.release_date
+      };
     });
-    movies = movies.sort(function(a,b){
-    return (new Date(a.release_date) - new Date(b.release_date));
-  });
+    movies = sortMovies(action.movies);
     return Object.assign(
       {},
       state,
@@ -73,9 +88,7 @@ const moviesReducer = (state=initialState, action) => {
     return Object.assign({}, state, {loading: true});
   }
   else if (action.type === actions.DELETE_MOVIE_SUCCESS) {
-            let movies = action.movies.sort(function(a,b){
-    return (new Date(a.release_date) - new Date(b.release_date));
-  });
+    let movies = sortMovies(action.movies);
     return Object.assign(
       {},
       state,
@@ -99,9 +112,7 @@ const moviesReducer = (state=initialState, action) => {
     return Object.assign({}, state, {loading: true});
   }
   else if (action.type === actions.ADD_MOVIE_SUCCESS) {
-            let movies = action.movies.sort(function(a,b){
-    return (new Date(a.release_date) - new Date(b.release_date));
-  });
+    let movies = sortMovies(action.movies);
     return Object.assign(
       {},
       state,
@@ -125,9 +136,7 @@ const moviesReducer = (state=initialState, action) => {
     return Object.assign({}, state, {loading: true});
   }
   else if (action.type === actions.GET_SEASONS_SUCCESS) {
-        let movies = action.movies.sort(function(a,b){
-    return (new Date(a.release_date) - new Date(b.release_date));
-  });
+    let movies = sortMovies(action.movies);
     return Object.assign(
       {},
       state,
@@ -150,32 +159,29 @@ const moviesReducer = (state=initialState, action) => {
     return Object.assign({}, state, {loading: true});
   }
   else if (action.type === actions.GET_EPISODES_SUCCESS) {
-        let movies = action.movies.sort(function(a,b){
-    return (new Date(a.release_date) - new Date(b.release_date));
-  });
-    console.log(movies);
-    return Object.assign(
-      {},
-      state,
-      {
-        loading: false,
-        error: null,
-        userMovies: movies,
-      }
-      );
-  }
-  else if (action.type === actions.GET_EPISODES_ERROR) {
-    return Object.assign(
-      {},
-      state,
-      {
-        loading: false,
-        error: action.error
-      }
-      );
-  }
+   let movies = sortMovies(action.movies);
+   return Object.assign(
+    {},
+    state,
+    {
+      loading: false,
+      error: null,
+      userMovies: movies,
+    }
+    );
+ }
+ else if (action.type === actions.GET_EPISODES_ERROR) {
+  return Object.assign(
+    {},
+    state,
+    {
+      loading: false,
+      error: action.error
+    }
+    );
+}
 
-  return state;
+return state;
 }
 
 export default moviesReducer;
