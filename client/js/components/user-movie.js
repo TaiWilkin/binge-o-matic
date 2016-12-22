d29f2334119b129bcd1d02d659a39c42152464de
Theresa Wilkin <theresawilkin@yahoo.com>
2016-12-22 09:42:35 -0500

    working but no seasons or episodes

--- client/js/components/user-movie.js
+++ client/js/components/user-movie.js
@@ -33,11 +33,11 @@ export class UserMovie extends React.Component {
       img = "";
     }
     if (this.props.media_type === 'tv') {
-      add = (<button onClick={this.addSeasons}>Seasons</button>);
+      // add = (<button onClick={this.addSeasons}>Seasons</button>);
 
     } 
     if (this.props.media_type === 'season') {
-      add = (<button onClick={this.addEpisodes}>Episodes</button>);
+      // add = (<button onClick={this.addEpisodes}>Episodes</button>);
       title = `${this.props.title}: Season ${this.props.number}`;
     } 
     
