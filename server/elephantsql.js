d80bbe748a6cf2c80e26d6a30729d6e17251f220
Theresa Wilkin <theresawilkin@yahoo.com>
2017-02-03 12:02:41 -0500

    update package.json

--- server/elephantsql.js
+++ server/elephantsql.js
@@ -1,10 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const KNEX_HOST = process.env.TEST_KNEX_HOST || process.env.KNEX_HOST;
+const KNEX_USER_DATABASE = process.env.TEST_KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE;
+const KNEX_PASSWORD = process.env.TEST_KNEX_PASSWORD || process.env.KNEX_PASSWORD;
+
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.TEST_KNEX_HOST || process.env.KNEX_HOST,
-    user: process.env.KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE,
-    password: process.env.KNEX_USER_DATABASE || process.env.KNEX_PASSWORD,
-    database: process.env.KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE
+    host: KNEX_HOST,
+    user: KNEX_USER_DATABASE,
+    password: KNEX_PASSWORD,
+    database: KNEX_USER_DATABASE
   },
   debug: true
 });
--- server/index.js
+++ server/index.js
@@ -6,15 +6,17 @@ import 'isomorphic-fetch';
 
 dotenv.config();
 
-console.log(process.env.KNEX_HOST)
+const KNEX_HOST = process.env.TEST_KNEX_HOST || process.env.KNEX_HOST;
+const KNEX_USER_DATABASE = process.env.TEST_KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE;
+const KNEX_PASSWORD = process.env.TEST_KNEX_PASSWORD || process.env.KNEX_PASSWORD;
 
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.TEST_KNEX_HOST || process.env.KNEX_HOST,
-    user: process.env.TEST_KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE,
-    password: process.env.TEST_KNEX_PASSWORD || process.env.KNEX_PASSWORD,
-    database: process.env.TEST_KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE
+    host: KNEX_HOST,
+    user: KNEX_USER_DATABASE,
+    password: KNEX_PASSWORD,
+    database: KNEX_USER_DATABASE
   },
   // debug: true,
   pool: { min: 0, max: 3 }
