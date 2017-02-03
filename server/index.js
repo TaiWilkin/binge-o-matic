7edf81f0d33eead4b66a0b4876fb28d06704b89d
Theresa Wilkin <theresawilkin@yahoo.com>
2017-02-03 11:57:26 -0500

    update package.json

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ import 'isomorphic-fetch';
 
 dotenv.config();
 
+console.log(process.env.KNEX_HOST)
+
 var knex = require('knex')({
   client: 'pg',
   connection: {
     host: process.env.TEST_KNEX_HOST || process.env.KNEX_HOST,
-    user: process.env.KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE,
-    password: process.env.KNEX_USER_DATABASE || process.env.KNEX_PASSWORD,
-    database: process.env.KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE
+    user: process.env.TEST_KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE,
+    password: process.env.TEST_KNEX_PASSWORD || process.env.KNEX_PASSWORD,
+    database: process.env.TEST_KNEX_USER_DATABASE || process.env.KNEX_USER_DATABASE
   },
   // debug: true,
   pool: { min: 0, max: 3 }
