801184768fb097826d36cab87e260d2ab03ff3d5
Theresa Wilkin <theresawilkin@yahoo.com>
2017-02-03 12:20:05 -0500

    knex fix

--- server/index.js
+++ server/index.js
@@ -6,17 +6,15 @@ import 'isomorphic-fetch';
 
 dotenv.config();
 
-const KNEX_HOST = process.env.KNEX_HOST;
-const KNEX_USER_DATABASE = process.env.KNEX_USER_DATABASE;
-const KNEX_PASSWORD = process.env.KNEX_PASSWORD;
+console.log(process.env.NODE_ENV);
 
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host: KNEX_HOST,
-    user: KNEX_USER_DATABASE,
-    password: KNEX_PASSWORD,
-    database: KNEX_USER_DATABASE
+    host: process.env.KNEX_HOST,
+    user: process.env.KNEX_USER_DATABASE,
+    password: process.env.KNEX_PASSWORD,
+    database: process.env.KNEX_USER_DATABASE
   },
   // debug: true,
   pool: { min: 0, max: 3 }
