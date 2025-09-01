d17f5f1c6d88440d83f809149392ff4dfbf8f983
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-08-31 20:00:18 -0400

    Remove unneeded client dependencies

--- client/__tests__/queries/Media.test.js
+++ client/__tests__/queries/Media.test.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
 import MediaQuery from "../../src/queries/Media.js";
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  optimizeDeps: {
+    exclude: ["graphql"],
+  },
+  ssr: {
+    external: ["graphql"],
+  },
   server: {
     proxy: {
       "/graphql": {
