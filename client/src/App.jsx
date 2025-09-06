d6ab5176b04af6846f2232339b7046bb8d1bfe60
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-09-06 11:33:18 -0400

    Optimize cache

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,11 @@
 import "./index.css";
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import cache from "./cache";
 import Root from "./components/Root";
-
-const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        list: {
-          keyArgs: ["id"],
-          read(existing, { args, toReference }) {
-            return (
-              existing ||
-              toReference({
-                __typename: "ListType",
-                id: args?.id,
-              })
-            );
-          },
-        },
-      },
-    },
-    ListType: {
-      keyFields: ["id"],
-      fields: {
-        media: {
-          keyArgs: false,
-          merge(existing = [], incoming = []) {
-            const merged = [...existing];
-            incoming.forEach((item) => {
-              if (!merged.some((ref) => ref.__ref === item.__ref)) {
-                merged.push(item);
-              }
-            });
-            return merged;
-          },
-        },
-      },
-    },
-  },
-});
 
 const client = new ApolloClient({
   uri: "/graphql",
